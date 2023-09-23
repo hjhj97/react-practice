dd6883b362d7065596789349131fe91577b78082
JuHeonHa <hjhjaueon97@naver.com>
2023-09-23 14:53:29 +0900

    feat : 무한스크롤

--- 01-todo-list/src/App.tsx
+++ 01-todo-list/src/App.tsx
@@ -1,19 +1,52 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+
+const LIMIT = 5;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const elem = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    let cancelled = false;
+    async function fetchPhotos() {
+      setIsLoading(true);
+      const res = await axios.get(`http://localhost:3000/photos?_page=${page}&_limit=${LIMIT}`);
+      setIsLoading(false);
+      if (!res) return;
+      if (cancelled) return;
+      setPhotos((prev) => [...prev, ...res.data]);
+    }
+    fetchPhotos();
+    return () => {
+      cancelled = true;
+    };
+  }, [page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (isLoading) return;
+      if (entries[0].isIntersecting) {
+        setPage((prev) => prev + 1);
+      }
+    });
+    observer.observe(elem!.current);
+  }, [isLoading]);
 
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        {photos.length > 0 &&
+          photos.map((photo) => (
+            <div key={photo.id}>
+              <img src={photo.thumbnailUrl} />
+              <h2>{photo.title}</h2>
+            </div>
+          ))}
+        <div ref={elem}>Observer</div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }

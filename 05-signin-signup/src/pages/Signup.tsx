18694c26322b68933f461c19292857de30f1a8bc
JuHeonHa <hjhjaueon97@naver.com>
2023-09-26 16:33:31 +0900

    feat : 회원가입 폼

--- 05-signin-signup/src/pages/Signup.tsx
+++ 05-signin-signup/src/pages/Signup.tsx
@@ -1,28 +1,65 @@
-import React, { FormEvent } from "react";
+import React, { ChangeEvent, FormEvent, useMemo, useState } from "react";
+import "../App.css";
 
 function Signup() {
+  const [signupData, setSignupData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const text = e.target.value;
+    setSignupData((prev) => ({ ...prev, [name]: text }));
+  };
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
+
+  const isNameValid = useMemo(() => {
+    return {
+      validLength: signupData.name.length >= 4 && signupData.name.length <= 10,
+      includeSpecialCharacter: signupData.name.includes("!"),
+    };
+  }, [signupData.name]);
+
   return (
     <div>
-      <h2>Signin</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <h2>Signup</h2>
+
+      <form onSubmit={handleSubmit} className="input-form">
+        <div className="input-field">
           <label htmlFor="name">name</label>
-          <input type="text" id="name" />
+          <div>
+            <input type="text" id="name" name="name" value={signupData.name} onChange={onChangeInput} />
+            <div>
+              <li className={`input-rule ${isNameValid.validLength && "valid"}`}>4글자 이상 10글자 이하</li>
+              <li className={`input-rule ${isNameValid.includeSpecialCharacter && "valid"}`}>특수문자 포함</li>
+            </div>
+          </div>
         </div>
-        <div>
+        <div className="input-field">
           <label htmlFor="name">email</label>
-          <input type="email" id="email" />
+          <div>
+            <input type="email" id="email" name="email" />
+            {true && <p className="error-text">Error</p>}
+          </div>
         </div>
-        <div>
+        <div className="input-field">
           <label htmlFor="password">password</label>
-          <input type="password" id="password" />
+          <div>
+            <input type="password" id="password" name="password" />
+            {true && <p className="error-text">Error</p>}
+          </div>
         </div>
-        <div>
+        <div className="input-field">
           <label htmlFor="confirm-password">confirm password</label>
-          <input type="password" id="confirm-password" />
+          <div>
+            <input type="password" id="confirm-password" />
+            {true && <p className="error-text">Error</p>}
+          </div>
         </div>
         <button type="submit">Signup</button>
       </form>

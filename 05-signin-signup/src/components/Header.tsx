b449b3e09f4936ed1b0f01aafc49e8289ca223ef
JuHeonHa <hjhjaueon97@naver.com>
2023-09-27 15:26:04 +0900

    feat : 로그인 구현

--- 05-signin-signup/src/components/Header.tsx
+++ 05-signin-signup/src/components/Header.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header style={{ display: "flex", gap: "1rem" }}>
-      <Link to="/">Signin</Link>
-      <Link to="/signup">Signup</Link>
+    <header style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <>
+          <Link to="/">Signin</Link>
+          <Link to="/signup">Signup</Link>
+        </>
+      </div>
     </header>
   );
 }
--- 05-signin-signup/src/pages/Signin.tsx
+++ 05-signin-signup/src/pages/Signin.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import { requestSignin } from "../api/auth";
 
 function Signin() {
-  return <div></div>;
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const onClickSignin = async () => {
+    const name = nameRef.current?.value ?? "";
+    const password = passwordRef.current?.value ?? "";
+    const res = await requestSignin(name, password);
+    if (res.success) {
+      localStorage.setItem("token", res.token);
+      localStorage.setItem("name", res.name);
+      alert("Login Success");
+    } else {
+      alert("Login Failed");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="name">name</label>
+        <input type="text" id="name" ref={nameRef} />
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input type="password" id="password" ref={passwordRef} />
+      </div>
+      <button onClick={onClickSignin}>Signin</button>
+    </div>
+  );
 }
 
 export default Signin;
